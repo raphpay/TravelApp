7fd1f1cc612f84e6b47da01efa9db7b605f79ecf
Raphael Payet <raphpay.dev@gmail.com>
2021-05-04 10:21:19 +0400

    Translation - Implement unit tests

--- TravelApp/Model/Translation/TranslationService.swift
+++ TravelApp/Model/Translation/TranslationService.swift
@@ -59,17 +59,23 @@ class TranslationService {
     
     private var task: URLSessionDataTask?
     private var session = URLSession(configuration: .default)
+    
+    init(session: URLSession) {
+        self.session = session
+    }
 
     
     // MARK: - Public functions
     func getTranslation(baseText: String, targetLanguage: Language, completion: @escaping ((_ success: Bool, _ translatedText: String?) -> Void)) {
         let completeStringURL = "https://translation.googleapis.com/language/translate/v2?key=\(API_KEY)&q=\(baseText)&target=\(targetLanguage.code)&format=text"
         let urlString = completeStringURL.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        print(completeStringURL)
         guard let url = URL(string: urlString!) else {
             completion(false, nil)
             return
         }
         let request = URLRequest(url: url)
+        task?.cancel()
         task = session.dataTask(with: request, completionHandler: { (_data, _response, _error) in
             DispatchQueue.main.async {
                 guard _error == nil else {
